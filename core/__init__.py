d21f3ddd88d546944fe6ef725f772be8e1a4f588
Joakim Lohikoski <joakim.lohikoski@gmail.com>
2022-07-13 16:24:11 +0300

    .

--- core/__init__.py
+++ core/__init__.py
@@ -1,15 +1,46 @@
-from flask import Flask, redirect, url_for, render_template
-from flask_cors import CORS as cors
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 
 app = Flask(__name__)
-cors(app)
+app.secret_key = "test"
+app.permanent_session_lifetime = timedelta(days=1)
 
 # app URL routing
 @app.route("/main/")
 def main():
-    return "<h1>Main page.</h1>"
-
+    return render_template("index.html")
 
 @app.route("/")
 def base_url_redirect():
     return redirect(url_for("main"))
+
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["name"]
+        session["user"] = user
+        flash("Login successful")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in")
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+
+@app.route("/logout/")
+def logout():
+    flash("Logout successful", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+@app.route("/user/")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in")
+        return redirect(url_for("login"))
+
