836cc1c74411f8ca8fbe5a78d4282dc73d537985
Joakim Lohikoski <joakim.lohikoski@gmail.com>
2023-03-05 21:01:14 +0200

    ProductList methods and adding/deleting queries.

--- core/app.py
+++ core/app.py
@@ -14,38 +14,48 @@ logger = lgm.get_logger(name=__name__)
 
 @app.route("/", methods=["GET"])
 def main():
-    # Temporary
-    products = request.args.get("products")
-    if not products:
+    if len(session_queries := session.get("queries", default=[])) != 0:
+        queries = []
+        for i in session_queries:
+            queries.append({
+                "name": i.name,
+                "count": i.count,
+                "quantity": i.quantity,
+                "unit": i.unit,
+                "category": i.category
+            })
+        session_queries = queries
+    if len(session_products := session.get("products", default=[])) != 0:
         products = []
-    session_store = session.get("store")
-    if session_store:
-        session_store = session_store[0]
-    message = request.args.get("message")
-    if not message:
-        message = ""
-    session_queries = session.get("queries")
-    if not session_queries:
-        session_queries = [{
-            "name": "Maito Laktoositon 1L",
-            "amount": 2,
-            "category": ""
-        },
-        {
-            "name": "Naudan Jauheliha 400g",
-            "amount": 1,
-            "category": ""
-        },
-        {
-            "name": "Chiquita Banaani",
-            "amount": 3,
-            "category": ""
-        }]
-    return render_template("index.html",
-                           products=products,
-                           message=message,
-                           session_store=session_store,
-                           session_queries=session_queries)
+        for i in session_products:
+            products.append({
+                "name": i.name,
+                "count": i.count,
+                "category": i.category,
+                "ean": i.ean,
+                "comparison_unit": i.comparison_unit,
+                "comparison_price": i.comparison_price,
+                "unit_price": i.unit_price,
+                "label_quantity": i.label_quantity,
+                "label_unit": i.label_unit
+            })
+        session_products = products
+    if not (message_state := session.get("message_state")):
+        message_state = "No current message."
+    if (session_store := session.get("store")):
+        store_state = session_store[0]
+    else:
+        store_state = "No store selected"
+    return render_template(
+        "index.html",
+        message_state=message_state,
+        store_state=store_state,
+        session_store=session_store,
+        session_products=session_products,
+        session_queries=session_queries,
+        queries_state=f"Queries length: {len(session_queries)}",
+        products_state=f"Products length: {len(session_products)}"
+    )
 
 
 @app.route("/get_store/", methods=["GET"])
@@ -66,40 +76,41 @@ def get_store():
                          flags=re.I | re.M)
     if store_query in (None, ""):
         logger.debug("Store query was empty, aborting query...")
-        return redirect(url_for(message="Store name cannot be empty.",
-                                endpoint="main"))
+        session["message_state"] = "Store name cannot be empty."
+        return redirect(url_for("main"))
     parsed_data = parse_store_from_string(string=store_query)
     if not any(parsed_data):
-        return redirect(url_for(message="ERROR when parsing store.",
-                                endpoint="main"))
+        session["message_state"] = "Error parsing store."
+        return redirect(url_for("main"))
     if (store := session.get("store")) and parsed_data[0] is not None:
         if store[0].lower() == parsed_data[0].lower():
             logger.debug(
                 "Store query was equal to session store, aborting query...")
-            return redirect(url_for(endpoint="main"))
+            return redirect(url_for("main"))
     store_data = execute_store_search(query_data=parsed_data)
     if not store_data:
         logger.debug("Returned store data was empty, returning 'not found'.")
-        return redirect(url_for(message="Store not found.",
-                                endpoint="main"))
+        session["message_state"] = "Store was not found."
+        return redirect(url_for("main"))
     session["store"] = store_data
     logger.debug("Set %s as session store.", store_data)
-    return redirect(url_for(endpoint="main"))
+    session["message_state"] = "Set new session store."
+    return redirect(url_for("main"))
 
 
 @app.route("/query/", methods=["POST"])
 def query():
     logger.info("Received a new product query!\n")
     if not (store := session.get("store")):
-        return redirect(url_for(endpoint="main"))
+        return redirect(url_for("main"))
 
     if not (query_data := process_queries(data=request.json)):
-        return redirect(url_for(endpoint="main"))
+        return redirect(url_for("main"))
     session["queries"] = query_data
     product_lists = execute_product_search(
         query_data=query_data,
         store=store,
         limit=20)
-    for i in product_lists:
-        print(i.products)
-    return redirect(url_for(endpoint="main"))
+    session["products"] = product_lists
+    session["message_state"] = "Product search successful."
+    return redirect(url_for("main"))
--- core/app_classes.py
+++ core/app_classes.py
@@ -1,5 +1,6 @@
 from utils import LoggerManager as lgm
 from dataclasses import dataclass, field
+from typing import Generator
 import core
 
 logger = lgm.get_logger(name=__name__)
@@ -44,40 +45,55 @@ class ProductList:
         if len(self.items) == 0:
             logger.debug("Parsing products for query: %s.",
                          self.query_item.name)
-            self.items = self._parse()
+            for i in self._parse():
+                self.items.append(i)
             logger.debug("Parsed %s products from response.",
                          len(self.items))
         return self.items
 
     @property
     def cheapest_item(self) -> ProductItem | None:
-        pass
-
-    def _parse(self) -> list[ProductItem]:
-        product_items = []
         if (response_items := self._get_response_items()) is not None:
-            for i in response_items:
-                quantity, unit = None, None
-                if (data := core.get_quantity_from_string(
-                        string=i["name"])) is not None:
-                    quantity, unit = data
-                try:
-                    item = ProductItem(
-                        name=i["name"],
-                        count=self.query_item.count,
-                        category=self.query_item.category,
-                        ean=i["ean"],
-                        store=self.store,
-                        comparison_unit=i["comparisonUnit"],
-                        comparison_price=i["comparisonPrice"],
-                        unit_price=i["price"],
-                        label_quantity=quantity,
-                        label_unit=unit)
-                    product_items.append(item)
-                except KeyError as err:
-                    logger.exception(err)
-                    continue
-        return product_items
+            values = (0, response_items[0])
+            for inx, item in enumerate(response_items[1:], start=1):
+                if item["comparisonPrice"] < values[1]["comparisonPrice"]:
+                    values = (inx, item)
+            item = self._create_product_item(item=values[1])
+            response_items.pop(values[0])
+            return item
+        return None
+
+    def _parse(self) -> Generator[ProductItem, None, None]:
+        if (response_items := self._get_response_items()) is None:
+            return None
+        for i in response_items:
+            item = self._create_product_item(item=i)
+            if not item:
+                continue
+            yield item
+        return None
+
+    def _create_product_item(self, item: dict) -> ProductItem | None:
+        try:
+            quantity, unit = None, None
+            if (data := core.get_quantity_from_string(
+                    string=item["name"])) is not None:
+                quantity, unit = data
+            product_item = ProductItem(
+                name=item["name"],
+                count=self.query_item.count,
+                category=item["hierarchyPath"][0]["slug"],
+                ean=item["ean"],
+                store=self.store,
+                comparison_unit=item["comparisonUnit"],
+                comparison_price=item["comparisonPrice"],
+                unit_price=item["price"],
+                label_quantity=quantity,
+                label_unit=unit)
+        except KeyError as err:
+            logger.exception(err)
+            return None
+        return product_item
 
     def _get_response_items(self) -> dict | None:
         try:
