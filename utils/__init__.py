5d6642db2d300292ed3ba0f98c85763d066db25d
Joakim Lohikoski <joakim.lohikoski@gmail.com>
2023-03-21 21:03:06 +0200

    Removed tests cause they need a rewrite anyway.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,15 @@
+"""Contains various utility functions and classes."""
 
 from .program_paths import Paths
 from .logger_manager import LoggerManager
-from .util_funcs import timer, regex_search, regex_findall
+from .util_funcs import timer
+from .util_funcs import regex_search
+from .util_funcs import regex_findall
 
 LoggerManager.configure(
     logs_path=Paths.logs(),
     keep_logs=False
 )
+
+__all__ = ["Paths", "LoggerManager", "timer",
+           "regex_search", "regex_findall"]
