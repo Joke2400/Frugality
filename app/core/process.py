8a5835f923737695b784c5e2abf71bcbedf36979
Joke2400 <joakim.lohikoski@gmail.com>
2023-10-17 08:28:07 +0300

    Expanded file structure, implemented db env vars

--- app/core/process.py
+++ app/core/process.py
@@ -1,19 +1,23 @@
 """Contains a process singleton class for managing the application."""
+import os
+from fastapi import FastAPI
 
-
-from fastapi import FastAPI, Response
 from ..api.routes import router
+from ..utils.patterns import SingletonMeta
 
-class Process():
+
+class Process(metaclass=SingletonMeta):
 
     app: FastAPI = FastAPI()
 
-    def __init__(self, **kwargs):
+    def __init__(self) -> None:
+        """Initialize app routes & fetch environment variables."""
+        if (db_user := os.getenv("POSTGRES_USER")) in (None, ""):
+            raise ValueError(
+                "ENVIRONMENT VAR 'POSTGRES_USER' WAS NOT SUPPLIED")
+        if (db_password := os.getenv("POSTGRES_PASSWORD")) in (None, ""):
+            raise ValueError(
+                "ENVIRONMENT VAR 'POSTGRES_PASSWORD' WAS NOT SUPPLIED")
+        self.db_user: str = str(db_user)
+        self.db_password: str = str(db_password)
         self.app.include_router(router)
-
-app = Process.app
-
-
-@app.get("/")
-def read_root():
-    return Response("This is a test")
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 """The starting point for the app."""
 import uvicorn
+from dotenv import load_dotenv
 
 from app.core import Process
 
+load_dotenv()
 process = Process()
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:process.app",
+        "main:process.Process.app",
         host="0.0.0.0",
         port=80,
         log_level="info",
