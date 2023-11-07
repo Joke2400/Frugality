b475052605c69db72f1b0ce6343ca1c91b44bb55
Joke2400 <joakim.lohikoski@gmail.com>
2023-11-07 08:56:43 +0200

    Added utility function for running debug code.

--- app/core/process.py
+++ app/core/process.py
@@ -4,20 +4,30 @@ from fastapi import FastAPI
 
 from app.api.routes.store import router
 from app.utils.patterns import SingletonMeta
+from app.utils.exceptions import MissingEnvironmentVar
 
 
 class Process(metaclass=SingletonMeta):
+    """Singleton for managing the execution of the entire app."""
 
     app: FastAPI = FastAPI()
 
     def __init__(self) -> None:
         """Initialize app routes & fetch environment variables."""
         if (db_user := os.getenv("POSTGRES_USER")) in (None, ""):
-            raise ValueError(
+            raise MissingEnvironmentVar(
                 "ENVIRONMENT VAR 'POSTGRES_USER' WAS NOT SUPPLIED")
         if (db_password := os.getenv("POSTGRES_PASSWORD")) in (None, ""):
-            raise ValueError(
+            raise MissingEnvironmentVar(
                 "ENVIRONMENT VAR 'POSTGRES_PASSWORD' WAS NOT SUPPLIED")
         self.db_user: str = str(db_user)
         self.db_password: str = str(db_password)
         self.app.include_router(router)
+        self._execute_debug_code()
+
+    @staticmethod
+    def _execute_debug_code():
+        """NOTE: THIS FUNCTION WILL NOT EXIST IN RELEASE VERSIONS."""
+        if os.getenv("DEBUG"):
+            from debug import run
+            run()
---
+++ app/utils/exceptions.py
@@ -0,0 +1,5 @@
+"""Contains custom exception classes."""
+
+
+class MissingEnvironmentVar(Exception):
+    """A required environment variable was not provided."""
