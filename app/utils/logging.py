aee4d7b4a80567987e3c4e662da15f6453297304
Joke2400 <joakim.lohikoski@gmail.com>
2023-11-23 10:55:20 +0200

    Added some docstrings to logging module.
    Renamed a parameter, removed & added todos.

--- app/utils/logging.py
+++ app/utils/logging.py
@@ -8,16 +8,30 @@ from app.utils.patterns import SingletonMeta, TreeNode
 
 Handler: TypeAlias = logging.StreamHandler | logging.FileHandler
 
-# TODO: LOGGING UTILITY CREATES TOO MANY LOG FILES 
-# (i did this on purpose for some unknown reason dont @ me)
-# TODO: Logs marked debug are logged to stream, probably
-# a misconfigured logger and/or log propagation
-# TODO: Refactoring, ex: get_logger() hard to read/understand
-
 
 class LoggerManager(metaclass=SingletonMeta):
-    """Class for managing logging."""
+    """Class for managing logging in the application.
 
+    Class uses a Singleton pattern. Configuration is to be set on
+    first call, upon subsequent calls, .__init__() does not run again.
+
+    Args:
+        log_path (Path | str):
+            The path to the directory where the logs should be stored.
+            Cannot be set to None upon first call to Singleton.
+
+        root_dir (Path | None):
+            Define a folder that should be considered the project root.
+            Defaults to the current working directory if set to None.
+
+        purge_old_logs (bool):
+            Set this to True if logs from previous runs should be purged.
+            Defaults to False.
+
+        default_format (logging.Formatter | None):
+            Set a custom default format. If this is set to None,
+            then the default format defined in the class is used instead.
+    """
     tree: TreeNode
     loggers: list[logging.Logger] = []
     default_format = logging.Formatter(
@@ -27,16 +41,16 @@ class LoggerManager(metaclass=SingletonMeta):
     def __init__(self, log_path: Path | str | None = None,
                  root_dir: Path | None = None,
                  purge_old_logs: bool = False,
-                 formatter: logging.Formatter | None = None) -> None:
+                 default_format: logging.Formatter | None = None) -> None:
         """Initialize an instance of LoggerManager.
 
-        Args:
-            log_path (Path | str):
-                The path to the directory where the logs should be saved.
-            purge_old_logs (bool, optional):
-                Set to True if logs from previous runs should be purged.
-                Defaults to False.
+            Set the log path, root directory & default_format.
+            Purge old logs if purge_old_logs is set to True
+            Configure root logger with the right configuration.
+            Initialize the logger tree with the root logger as the first node.
         """
+        if default_format is not None:
+            self.default_format = default_format
         if log_path is None:
             raise ValueError(
                 "Log path must be provided on first call to class.")
@@ -54,7 +68,8 @@ class LoggerManager(metaclass=SingletonMeta):
         # quick and dirty fix to ensure root logger doesn't log to stream
         for i in self.root_logger.handlers:
             self.root_logger.removeHandler(i)
-        config = self.create_config(logging.INFO, logging.DEBUG, formatter)
+        config = self.create_config(
+            logging.INFO, logging.DEBUG, self.default_format)
         config["file"]["filename"] = self.log_path / "root.log"
         self.root_logger = self.create_logger(
             name="root",
@@ -65,13 +80,40 @@ class LoggerManager(metaclass=SingletonMeta):
                   "logger": self.root_logger,
                   "path": self.root_dir})
 
-    def get_logger(self, path: str, name: str | None = None,
-                   sh: int = 0, fh: int = 0,
-                   formatter: logging.Formatter | None = None
+    def get_logger(self, path: str,
+                   sh: int, fh: int,
+                   formatter: logging.Formatter | None = None,
+                   name: str | None = None,
                    ) -> logging.Logger:
-        """Get a new logger. TODO: IMPLEMENT CUSTOM NAME"""
+        """Get a new logger instance via the manager.
+
+        Args:
+            path (str):
+                IMPORTANT: set this parameter to __name__.
+                Path to the file that this call is coming from.
+
+            sh (int):
+                The level that the logger streamhandler should be set to.
+
+            fh (int):
+                The level that the logger filehandler should be set to.
+
+            formatter (logging.Formatter | None):
+                A custom formatter that the logger should use.
+                Uses default_format defined in class if not provided.
+
+            name (str | None, optional):
+                TODO: IMPLEMENT THIS.
+
+        Raises:
+            ValueError:
+                Raised if the path provided does not exist.
+
+        Returns:
+            logging.Logger: Returns a configured logger instance.
+        """
         rel_path = Path(path.replace(".", "/") + ".py")
-        if not (Path.cwd() / rel_path).exists():
+        if not (self.root_dir / rel_path).exists():
             raise ValueError(
                 "Could not construct a valid logger path from path.")
         config = self.create_config(sh, fh, formatter)
@@ -81,6 +123,7 @@ class LoggerManager(metaclass=SingletonMeta):
 
     def create_logger_tree(self, rel_path: Path, config: dict,
                            log_file_granularity: int = 2) -> TreeNode:
+        """TODO: Proper docstring for this one"""
         node = self.tree
         for index, part_name in enumerate(rel_path.parts, start=1):
             # Check part_name doesn't exist in current node children.
@@ -97,7 +140,7 @@ class LoggerManager(metaclass=SingletonMeta):
                 # -> Create that node -> Use the node in next loop
                 rel_path_to_logger = Path(
                     "/".join(rel_path.parts[0:index]))
-                abs_path_to_logger = Path.cwd() / rel_path_to_logger
+                abs_path_to_logger = self.root_dir / rel_path_to_logger
                 child_config = config.copy()
                 if part_name.endswith(".py"):
                     part_name = part_name.removesuffix(".py")
