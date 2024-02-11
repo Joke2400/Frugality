cd27272eefb01bb46a9490f6650e3aeb7b5981d0
Joakim Lohikoski <joakim.lohikoski@gmail.com>
2024-02-11 15:17:54 +0200

    Removed files created by npm created simple reactive template to verify vue works.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue"
 
 const app = createApp(App)
-
-app.use(router)
 
 app.mount('#app')
