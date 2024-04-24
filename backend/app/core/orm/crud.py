50934ebb19b2851e0071d12e0c6a2f61805e8e8b
Joakim Lohikoski <joakim.lohikoski@gmail.com>
2024-04-24 19:46:09 +0300

    Moved db queries to operations.py, cleaned up dead code.

--- backend/app/core/orm/crud.py
+++ backend/app/core/orm/crud.py
@@ -1,15 +1,11 @@
 """Contains CRUD operations for interaction with the database."""
-from typing import Type, TypeVar, cast as cast_type
-from sqlalchemy import select, insert as sql_insert
+from typing import Type, TypeVar
+from sqlalchemy import insert as sql_insert
 from sqlalchemy.sql import Select
-from pydantic import ValidationError
 
 from backend.app.core.typedefs import SchemaOut, OrmModel
 from backend.app.core.orm.database import SessionContext, Base
 from backend.app.utils import LoggerManager
-
-from . import models
-from . import schemas
 
 logger = LoggerManager().get_logger(__name__, sh=0, fh=10)
 
@@ -102,79 +98,3 @@ def read_all(stmt: Select, session_ctx: SessionContext) -> list[OrmModel]:
     with session_ctx:
         return session_ctx.session.scalars(stmt).all()
     return []
-
-
-def select_all_join[SchemaT: SchemaOut](
-        stmt: Select,
-        table_cast: Type[SchemaT],
-        join_cast: Type[SchemaT],
-        ) -> tuple[SchemaT, SchemaT]:
-    result: list[tuple[SchemaT, SchemaT]] = []
-    with database.DBContext(read_only=True) as context:
-        print(len(context.session.execute(stmt).all()))
-    return None
-
-# ---- STORE GET FUNCTIONS ----
-def get_store_by_id(store_id: int) -> schemas.StoreDB | None:
-    """Get a store by id."""
-    stmt = (
-        select(models.Store)
-        .where(models.Store.store_id == store_id)
-    )
-    return select_one(stmt=stmt, cast=schemas.StoreDB)
-
-
-def get_store_by_slug(slug: str) -> schemas.StoreDB | None:
-    """Get a store by slug."""
-    stmt = (
-        select(models.Store)
-        .where(models.Store.slug == slug)
-    )
-    return select_one(stmt=stmt, cast=schemas.StoreDB)
-
-
-def get_stores_by_name(
-        name: str, brand: str | None = None) -> list[schemas.StoreDB]:
-    """Get stores by name."""
-    stmt = (
-        select(models.Store)
-        .where(models.Store.store_name.ilike(
-            f"%{name}%"))
-    )
-    if brand is not None:
-        stmt = stmt.where(models.Store.brand == brand)
-    stmt = stmt.order_by(models.Store.store_name)
-    return select_all(stmt=stmt, cast=schemas.StoreDB)
-
-# ---- PRODUCT GET FUNCTIONS ----
-def get_product_by_ean(ean: str) -> schemas.ProductDB | None:
-    """Get a store by ean."""
-    stmt = (
-        select(models.Product)
-        .where(models.Product.ean == ean)
-    )
-    return select_one(stmt=stmt, cast=schemas.ProductDB)
-
-
-def get_product_by_slug(slug: str) -> schemas.ProductDB | None:
-    """Get a product by slug."""
-    stmt = (
-        select(models.Product)
-        .where(models.Product.slug == slug)
-    )
-    return select_one(stmt=stmt, cast=schemas.ProductDB)
-
-
-def get_products_by_name(
-        name: str, category: str | None = None) -> list[schemas.ProductDB]:
-    """Get products by name."""
-    stmt = (
-        select(models.Product)
-        .where(models.Product.name.ilike(
-            f"%{name}%"
-        ))
-    )
-    if category is not None:
-        stmt = stmt.where(models.Product.category == category)
-    stmt = stmt.order_by(models.Product.name)
-    return select_all(stmt=stmt, cast=schemas.ProductDB)
--- backend/app/core/orm/operations.py
+++ backend/app/core/orm/operations.py
@@ -2,39 +2,73 @@ from typing import Type
 from sqlalchemy import select
 from pydantic import ValidationError
 from backend.app.core.orm.database import SessionContext
-from backend.app.core.orm import models, schemas, crud
+from backend.app.core.orm import models, schemas, crud, database
 from backend.app.core.typedefs import SchemaOut, SchemaIn, OrmModel
 from backend.app.utils import LoggerManager
 
 logger = LoggerManager().get_logger(__name__, sh=0, fh=10)
 
 
-def convert_to_model[ModelT: OrmModel](
-        schema: SchemaIn, model: Type[ModelT]) -> ModelT | None:
+def get_store_by_id(store_id: int) -> schemas.StoreDB | None:
+    stmt = (
+        select(models.Store)
+        .where(models.Store.store_id == store_id)
+    )
+    ctx = database.ORM().get_session_context()
+    result = crud.read_one(stmt=stmt, session_ctx=ctx)
     try:
-        return model(**dict(schema))
-    except ValueError as err:
-        logger.debug(err)
-    return None
-
-
-def convert_to_schema[SchemaT: SchemaOut](
-        model: OrmModel, schema: Type[SchemaT]) -> SchemaT | None:
-    try:
-        return schema.model_validate(model)
+        return schemas.StoreDB.model_validate(result)
     except ValidationError as err:
         logger.debug(err)
     return None
 
 
-def get_store_by_id(store_id: int, context: SessionContext) -> schemas.StoreDB | None:
+def get_stores_by_name(name: str, brand: str) -> list[schemas.StoreDB]:
     stmt = (
         select(models.Store)
-        .where(models.Store.store_id == store_id)
+        .where(models.Store.store_name.ilike(
+            f"%{name}%"))
     )
-    with context:
-        if (result := crud.select_one(stmt, session=context.session)) is None:
-            return None
-        item = convert_to_schema(
-            model=result, schema=schemas.StoreDB)
-    return item
+    if brand is not None:
+        stmt = stmt.where(models.Store.brand == brand)
+    stmt = stmt.order_by(models.Store.store_name)
+    ctx = database.ORM().get_session_context()
+    result = crud.read_all(stmt=stmt, session_ctx=ctx)
+    try:
+        return [schemas.StoreDB.model_validate(i) for i in result]
+    except ValidationError as err:
+        logger.debug(err)
+    return []
+
+
+def get_product_by_ean(ean: str) -> schemas.ProductDB | None:
+    stmt = (
+        select(models.Product)
+        .where(models.Product.ean == ean)
+    )
+    ctx = database.ORM().get_session_context()
+    result = crud.read_one(stmt=stmt, session_ctx=ctx)
+    try:
+        return schemas.ProductDB.model_validate(result)
+    except ValidationError as err:
+        logger.debug(err)
+    return None
+
+
+def get_products_by_name(name: str, category: str) -> list[schemas.ProductDB]:
+    stmt = (
+        select(models.Product)
+        .where(models.Product.name.ilike(
+            f"%{name}%"
+        ))
+    )
+    if category is not None:
+        stmt = stmt.where(models.Product.category == category)
+    stmt = stmt.order_by(models.Product.name)
+    ctx = database.ORM().get_session_context()
+    result = crud.read_all(stmt=stmt, session_ctx=ctx)
+    try:
+        return [schemas.ProductDB.model_validate(i) for i in result]
+    except ValidationError as err:
+        logger.debug(err)
+    return []
